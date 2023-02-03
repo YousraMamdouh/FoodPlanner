c4ab2f162b00bbbb2aa70bcb1cdca243af9a74ad
YousraMamdouh <yousramamdouh98@gmail.com>
2023-02-03 23:37:35 +0200

    adding feature(Main search screen/ Category search/cuisine search / ingredient search

--- app/src/main/java/com/example/foodplanner/searchByIngredient/View/SearcByhIngridientFragment.java
+++ app/src/main/java/com/example/foodplanner/searchByIngredient/View/SearcByhIngridientFragment.java
@@ -61,6 +61,6 @@ public class SearcByhIngridientFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_searc_byh_ingridient, container, false);
+        return inflater.inflate(R.layout.fragment_searc_byh_ingredient, container, false);
     }
 }
