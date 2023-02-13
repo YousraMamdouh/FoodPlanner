08a75e3dbaa9048913bee49c87def3cd7cc0bec6
YousraMamdouh <yousramamdouh98@gmail.com>
2023-02-13 13:04:42 +0200

    AllMealsAdapter class added

---
+++ app/src/main/java/com/example/foodplanner/search/View/AllMealsAdapter.java
@@ -0,0 +1,87 @@
+package com.example.foodplanner.search.View;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.foodplanner.R;
+import com.example.foodplanner.model.MealsDetails;
+
+import java.util.List;
+
+public class AllMealsAdapter extends RecyclerView.Adapter<AllMealsAdapter.MyViewHolder>{
+
+    private static final String Tag="All Meals Adapter";
+    private List<MealsDetails> allMealsList;
+    private Context context;
+
+    public void setAllMealsItemList(List<MealsDetails> allMealsList) {
+        this.allMealsList =allMealsList;
+    }
+
+
+   // private OnFavoriteClickListener listener;
+
+
+    public AllMealsAdapter(Context context, List<MealsDetails> allMealsList)
+    {
+        this.context=context;
+        this.allMealsList = allMealsList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view =inflater.inflate(R.layout.search_all_meals,parent,false);
+
+        MyViewHolder myViewHolder=new MyViewHolder(view);
+        Log.i(Tag,"OnCreateViewHolder");
+        return myViewHolder;
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        MealsDetails currentProduct = allMealsList.get(position);
+        holder.mealName.setText(currentProduct.getStrMeal());
+        //Log.i(Tag,"the category: "+currentProduct.getStrCategory());
+       // System.out.println("My name is : "+currentProduct.getIdMeal());
+       holder.mealCountry.setText(currentProduct.getStrArea());
+        Glide.with(context).load(currentProduct.getStrMealThumb())
+                .apply(new RequestOptions()
+                        .override(150,150)).into(holder.mealImage);
+        Log.i(Tag,"onBindViewHolder");
+    }
+
+    @Override
+    public int getItemCount() {
+        return allMealsList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mealName;
+        TextView mealCountry;
+        ImageView mealImage;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mealImage=itemView.findViewById(R.id.mealImageView);
+            mealName=itemView.findViewById(R.id.mealName);
+            mealCountry=itemView.findViewById(R.id.mealCountry);
+
+        }
+    }
+}
